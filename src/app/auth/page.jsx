58cf90b4b72ee745e8c3ea1bad37ee8f47b0fb6f
Franco Buceta <francobuceta95@gmail.com>
2023-06-27 17:46:24 -0300

    add navbar

--- src/app/auth/page.jsx
+++ src/app/auth/page.jsx
@@ -36,7 +36,7 @@ const Auth = () => {
             <input type="text" placeholder="Name" />
             <input type="email" placeholder="Email" />
             <input type="password" placeholder="Password" />
-            <button className="rounded-[20px] bg-primary text-xs font-bold py-3 px-11 transition-transform duration-[80ms] ease-in active:scale-[0.95]">
+            <button className="rounded-[20px] bg-[#C0FF6B] text-xs text-[#292929] font-bold py-3 px-11 transition-transform duration-[80ms] ease-in active:scale-[0.95]">
               SIGN UP
             </button>
           </form>
@@ -58,7 +58,7 @@ const Auth = () => {
             <input type="email" placeholder="Email" />
             <input type="password" placeholder="Password" />
             <a href="#">Forgot your password?</a>
-            <button className="rounded-[20px] bg-primary text-xs font-bold py-3 px-11 transition-transform duration-[80ms] ease-in active:scale-[0.95]">
+            <button className="rounded-[20px] bg-[#C0FF6B] text-xs text-[#292929] font-bold py-3 px-11 transition-transform duration-[80ms] ease-in active:scale-[0.95]">
               SIGN IN
             </button>
           </form>
@@ -74,7 +74,7 @@ const Auth = () => {
               <p className="text-sm my-5">
                 To keep connected with us please login with your personal info
               </p>
-              <button className="rounded-[20px] border border-solid border-white bg-transparent text-xs font-bold py-3 px-11 transition-transform duration-[80ms] ease-in active:scale-[0.95]" 
+              <button className="rounded-[20px] border border-solid border-white bg-transparent text-xs text-lightblack font-bold py-3 px-11 transition-transform duration-[80ms] ease-in active:scale-[0.95]" 
                 id="signIn" onClick={handleClick}>
                 SIGN IN
               </button>
@@ -84,7 +84,7 @@ const Auth = () => {
               <p className="text-sm my-5">
                 Enter your personal details and start journey with us
               </p>
-              <button className="rounded-[20px] border border-solid border-white bg-transparent text-xs font-bold py-3 px-11 transition-transform duration-[80ms] ease-in active:scale-[0.95]" 
+              <button className="rounded-[20px] border border-solid border-white bg-transparent text-xs text-lightblack font-bold py-3 px-11 transition-transform duration-[80ms] ease-in active:scale-[0.95]" 
                 id="signUp" onClick={handleClick}>
                 SIGN UP
               </button>
--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,30 +1,5 @@
-import Link from "next/link";
-
-const links = [
-  {
-    label: "Inicio",
-    route: "/",
-  },
-  {
-    label: "Acceso",
-    route: "/auth",
-  },
-];
-
-const Navbar = () => {
-  return (
-    <header>
-      <nav>
-        <ul>
-          {links.map(({ label, route }) => (
-            <li key={route}>
-              <Link href={route}>{label}</Link>
-            </li>
-          ))}
-        </ul>
-      </nav>
-    </header>
-  );
+const Home = () => {
+  return null;
 };
 
-export default Navbar;
+export default Home;
