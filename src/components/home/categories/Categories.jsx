2d089e0f6dd5eb036630a6d1d37733ceba1fe4ab
Franco Buceta <francobuceta95@gmail.com>
2023-06-28 12:23:55 -0300

    add categories names

--- src/components/home/categories/Categories.jsx
+++ src/components/home/categories/Categories.jsx
@@ -1,52 +1,73 @@
 "use client"
-
 import { Swiper, SwiperSlide } from 'swiper/react';
+import Link from 'next/link';
 import 'swiper/css';
 
 const categories = [
     {
-        title: "Category",
-        img: "https://picsum.photos/200"
+        title: "Teclados",
+        img: "https://picsum.photos/200",
+        url: "/"
     },
     {
-        title: "Category",
-        img: "https://picsum.photos/200"
+        title: "Mouses",
+        img: "https://picsum.photos/200",
+        url: "/"
     },
     {
-        title: "Category",
-        img: "https://picsum.photos/200"
+        title: "Auriculares",
+        img: "https://picsum.photos/200",
+        url: "/"
     },
     {
-        title: "Category",
-        img: "https://picsum.photos/200"
+        title: "Pads",
+        img: "https://picsum.photos/200",
+        url: "/"
     },
     {
-        title: "Category",
-        img: "https://picsum.photos/200"
+        title: "Cámaras Web",
+        img: "https://picsum.photos/200",
+        url: "/"
+    },
+    {
+        title: "Volantes y Gamepads",
+        img: "https://picsum.photos/200",
+        url: "/"
+    },
+    {
+        title: "Parlantes y Audio",
+        img: "https://picsum.photos/200",
+        url: "/"
     },
 ]
 
 const Categories = () => {
     return (
-        <Swiper
-            spaceBetween={50}
-            slidesPerView={3}
-            onSlideChange={() => console.log('slide change')}
-            onSwiper={(swiper) => console.log(swiper)}
-        >
+        <section className='mt-5'>
+            <Swiper
+                spaceBetween={0}
+                slidesPerView={4}
+                onSlideChange={() => console.log('slide change')}
+                onSwiper={(swiper) => console.log(swiper)}
+            >
 
-            {
-                categories.map(elem => {
-                    return (
-                        <SwiperSlide>
-                            <img src={elem.img} alt="" />
-                            <span>{elem.title}</span>
-                        </SwiperSlide>
-                    )
-                })
-            }
-            ...
-        </Swiper>
+                {
+                    categories.map(elem => {
+                        return (
+                            <SwiperSlide>
+                                <Link href={elem.url}>
+                                    <div className='flex flex-col items-center gap-3'>
+                                        <img src={elem.img} alt={elem.title} className="rounded-full w-[200px]" />
+                                        <span>{elem.title}</span>
+                                    </div>
+                                </Link>
+                            </SwiperSlide> 
+                        )
+                    })
+                }
+
+            </Swiper>
+        </section>
     )
 }
 
