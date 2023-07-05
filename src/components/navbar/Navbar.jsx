553d7d5ccc9276d851fb1f43379d156e430e2ab0
Franco Buceta <francobuceta95@gmail.com>
2023-07-05 12:17:53 -0300

    change navbar icons extent

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -6,13 +6,13 @@ const Navbar = () => {
   return (
     <header className="navbar bg-custome-secondary sm:px-32 p-8">
       <Logo />
-      <div className="flex items-center gap-2">
+      <div className="flex items-center gap-6">
         <div className="dropdown dropdown-end">
           <label tabIndex={0} className="btn btn-ghost btn-circle">
             <div className="indicator">
               <svg
                 xmlns="http://www.w3.org/2000/svg"
-                className="h-6 w-6 mr-2"
+                className="h-8 w-8 mr-2"
                 fill="none"
                 viewBox="0 0 24 24"
                 stroke="#BAFF29"
@@ -24,7 +24,7 @@ const Navbar = () => {
                   d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z"
                 />
               </svg>
-              <span className="badge badge-sm indicator-item text-white">
+              <span className="badge badge-sm indicator-item text-white text-sm font-main">
                 8
               </span>
             </div>
@@ -44,7 +44,7 @@ const Navbar = () => {
         </div>
         <div className="dropdown dropdown-end">
           <label tabIndex={0}>
-            <VscAccount fontSize={25} color="white" />
+            <VscAccount fontSize={30} color="white" />
           </label>
           <ul
             tabIndex={0}
