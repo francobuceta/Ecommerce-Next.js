2c972813ab6e29297d877ff71ba1aeee8fad2bab
Franco Buceta <francobuceta95@gmail.com>
2023-07-05 19:35:20 -0300

    add responsive to hero

--- src/components/home/Hero.jsx
+++ src/components/home/Hero.jsx
@@ -2,19 +2,19 @@ import Image from "next/image";
 
 const Hero = () => {
     return (
-        <section className="w-full h-[600px] bg-custome-secondary pt-10">
-            <div className="relative flex justify-center items-center gap-5 p-12 bg-[#BAFF29] rounded-l-3xl ml-20 h-[500px]">
+        <section className="w-full h-[600px] bg-custome-secondary sm:pt-10">
+            <div className="sm:relative flex justify-center items-center gap-5 p-0 sm:p-12 bg-[#BAFF29] sm:rounded-l-3xl sm:ml-20 h-[350px] sm:h-[400px] lg:h-[500px]">
                 
-                <div className="flex flex-col w-[40%]">
-                    <p className="absolute text-[6.5rem] leading-[6rem] font-bold text-custome-secondary left-20 top-12">
-                        THE <br /> NEXT GENENERA <br /> TECH
+                <div className="relative sm:static flex flex-col items-center sm:items-start w-full sm:w-[40%]">
+                    <p className="absolute text-center sm:text-left text-5xl md:text-6xl lg:text-[6.5rem] leading-[4rem] lg:leading-[6rem] font-bold text-custome-secondary sm:left-20 2xl:left-36 sm:top-12">
+                        YOUR <br className="hidden sm:block"/> GATEWAY TO <br /> PROGRESS
                     </p>
-                    <button className="btn btn-active bg-custome-secondary text-xl text-white w-40 h-10 mt-72 ml-10">
+                    <button className="btn btn-active bg-custome-secondary text-xl text-white w-40 h-10 mt-40 sm:mt-60 md:mt-72 sm:ml-10">
                         Ingresar aquí
                     </button>
                 </div>
 
-                <div>
+                <div className="hidden sm:block">
                     <Image
                         src="/img/headphones3.png"
                         width={0}
