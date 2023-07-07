a4df084ceba01f67bc567aaa00b4f75a32838c99
Franco Buceta <francobuceta95@gmail.com>
2023-07-07 19:07:44 -0300

    add card product component

--- src/app/products/page.jsx
+++ src/app/products/page.jsx
@@ -1,7 +1,13 @@
 import CategorySelect from "@/components/products/CategorySelect";
+import ProductCard from "@/components/products/ProductCard";
 
 const Products = () => {
-  return <CategorySelect />;
+  return (
+    <>
+      <CategorySelect />
+      <ProductCard />
+    </>
+  );
 };
 
 export default Products;
