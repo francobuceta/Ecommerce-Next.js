65021cd6b11ce62ea9b1ed85eaeaaf0a8558da67
Franco Buceta <francobuceta95@gmail.com>
2023-07-07 11:05:55 -0300

    add about us section

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,5 +1,6 @@
 import Hero from "@/components/home/Hero";
 import Categories from "@/components/home/Categories";
+import AboutUs from "@/components/home/AboutUs";
 import BestSelling from "@/components/home/BestSelling";
 
 const Home = () => {
@@ -7,6 +8,7 @@ const Home = () => {
     <>
       <Hero />
       <Categories />
+      <AboutUs />
       <BestSelling />
     </>
   );
