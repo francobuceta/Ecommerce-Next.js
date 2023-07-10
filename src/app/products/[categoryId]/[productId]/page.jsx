c6a13d34fcc3fb58b74e6453bbd75f3db8ebf2bf
Franco Buceta <francobuceta95@gmail.com>
2023-07-10 10:45:26 -0300

    add product id folder

---
+++ src/app/products/[categoryId]/[productId]/page.jsx
@@ -0,0 +1,7 @@
+const ProductDetail = ({ params }) => {
+    const { productId } = params;
+  
+    return <h1 className="text-white">Detalle del producto {productId}</h1>;
+  };
+  
+  export default ProductDetail;
