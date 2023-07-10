cdee0b52ff9e5801f0786cb4c5349b0c16e14d1d
Franco Buceta <francobuceta95@gmail.com>
2023-07-10 16:43:19 -0300

    fetch products from backend

--- src/app/products/page.jsx
+++ src/app/products/page.jsx
@@ -1,63 +1,21 @@
 import ProductCard from "@/components/products/ProductCard";
 
-const products = [
-  {
-    title: "Silla Corsair T1 RACE",
-    description: "Ergonomía | Comodidad | Ajustabilidad",
-    img: "/img/chair-corsair.jpeg",
-    url: "/",
-    price: "40.000",
-  },
-  {
-    title: "Auricular Redragon Lamia 2",
-    description: "Calidad | Portabilidad | Aislamiento",
-    img: "/img/headphone-dragon.jpeg",
-    url: "/",
-    price: "40.000",
-  },
-  {
-    title: "Teclado Galax Stealth 03",
-    description: "Sensibilidad | Comodidad | Ergonomía",
-    img: "/img/keyboard-galax.jpeg",
-    url: "/",
-    price: "40.000",
-  },
-  {
-    title: "Mouse Logitech G502X",
-    description: "Precisión | Durabilidad | Conectividad",
-    img: "/img/mouse-logi.jpeg",
-    url: "/",
-    price: "40.000",
-  },
-  {
-    title: "Parlante Logitech Z607",
-    description: "Conectividad | Potencia | Durabilidad",
-    img: "/img/phone-speaker-logi.jpeg",
-    url: "/",
-    price: "40.000",
-  },
-  {
-    title: "Barra De Sonido Noblex 2.1",
-    description: "Potencia | Estética | Calidad",
-    img: "/img/phone-speaker-noblex.jpeg",
-    url: "/",
-    price: "40.000",
-  },
-  {
-    title: "Volante Logitech G29",
-    description: "Sensibilidad | Durabilidad | Precisión",
-    img: "/img/steering-wheel-logi.jpeg",
-    url: "/",
-    price: "40.000",
-  },
-];
+const fetchProducts = async () => {
+  const response = await fetch(`${process.env.BACKEND_API_URL}/api/products`, {
+    next: { revalidate: 60 }
+  });
+  const data = await response.json();
+  return data;
+};
 
-const Products = () => {
+const Products = async () => {
+  const productsBack = await fetchProducts();
+
   return (
     <section className="flex justify-center flex-wrap gap-8 mt-12">
-      {products.map((product) => (
+      {productsBack.payload.map((product) => (
         <ProductCard
-          image={product.img}
+          image={product.image}
           title={product.title}
           description={product.description}
           price={product.price}
--- src/components/products/ProductCard.jsx
+++ src/components/products/ProductCard.jsx
@@ -7,22 +7,22 @@ const ProductCard = ({ image, title, description, price }) => {
       className="card card-compact w-72 h-[400px] bg-base-100 shadow-xl"
       key={title}
     >
-      <figure className="relative">
+      <figure className="relative p-20">
         <Image
           src={image}
           width={0}
           height={0}
           sizes="100vw"
           alt="Headphone"
-          className="w-full h-auto"
+          className="w-full h-auto object-contain"
         />
         <FavouriteButton title={title} />
       </figure>
 
       <div className="card-body bg-custome-primary rounded-b-2xl gap-0">
-        <h3 className="card-title">{title}</h3>
-        <p>{description}</p>
-        <span className="text-xl font-bold">{price}</span>
+        <h3 className="card-title select-none">{title}</h3>
+        <p className="truncate">{description}</p>
+        <span className="text-xl font-bold">$ {price}</span>
         <div className="card-actions justify-end">
           <button className="btn bg-transparent border border-custome-secondary w-20">
             Ver
