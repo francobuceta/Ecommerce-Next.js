acd45b47aca685b70eb7953f55a4ca1f36248ac4
Franco Buceta <francobuceta95@gmail.com>
2023-07-11 15:00:43 -0300

    add pagination buttons

--- src/app/products/page.jsx
+++ src/app/products/page.jsx
@@ -3,7 +3,9 @@ import { getRequest } from "@/services/serverFetching";
 
 const Products = async () => {
   const productsBack = await getRequest("/api/products");
-  
+
+  //const pagination = () => {};
+
   return (
     <section className="flex justify-center flex-wrap gap-8 mt-12">
       {productsBack.payload.map((product) => (
@@ -15,6 +17,17 @@ const Products = async () => {
           price={product.price}
         />
       ))}
+
+      {productsBack.info.totalPages > 1 && (
+        <div className="join grid grid-cols-2 mt-10">
+          <button className="join-item btn btn-outline bg-white">
+            Anterior
+          </button>
+          <button className="join-item btn btn-outline bg-white">
+            Siguiente
+          </button>
+        </div>
+      )}
     </section>
   );
 };
