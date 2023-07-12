78fa7734dfa5ec1b0510532b932c231e62a0cea7
Franco Buceta <francobuceta95@gmail.com>
2023-07-12 16:24:10 -0300

    add pagination functionality

--- src/app/products/page.jsx
+++ src/app/products/page.jsx
@@ -1,33 +1,69 @@
+"use client";
+import { useState, useEffect } from "react";
 import ProductCard from "@/components/products/ProductCard";
-import { getRequest } from "@/services/serverFetching";
+import PaginationButton from "@/components/products/PaginationButton";
+import { getRequest } from "@/services/clientFetching";
 
-const Products = async () => {
-  const productsBack = await getRequest("/api/products");
+const Products = () => {
+  const [products, setProducts] = useState(null);
+  const [currentPage, setCurrentPage] = useState(1);
 
-  //const pagination = () => {};
+  const handlePrevPage = () => {
+    if (currentPage > 1) {
+      setCurrentPage(currentPage - 1);
+    }
+  };
+
+  const handleNextPage = () => {
+    if (products.info.hasNextPage) {
+      setCurrentPage(currentPage + 1);
+    }
+  };
+
+  const fetchData = async (page) => {
+    try {
+      const response = await getRequest(`/api/products?page=${page}`);
+      setProducts(response);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    fetchData(currentPage);
+    window.scrollTo({top:0, behavior:"smooth"});
+  }, [currentPage]);
 
   return (
     <section className="flex justify-center flex-wrap gap-8 mt-12">
-      {productsBack.payload.map((product) => (
-        <ProductCard
-          key={product._id}
-          image={product.image}
-          title={product.title}
-          description={product.description}
-          price={product.price}
-        />
-      ))}
-
-      {productsBack.info.totalPages > 1 && (
-        <div className="join grid grid-cols-2 mt-10">
-          <button className="join-item btn btn-outline bg-white">
-            Anterior
-          </button>
-          <button className="join-item btn btn-outline bg-white">
-            Siguiente
-          </button>
-        </div>
+      {products ? (
+        products.payload.map((product) => (
+          <ProductCard
+            key={product._id}
+            image={product.image}
+            title={product.title}
+            description={product.description}
+            price={product.price}
+          />
+        ))
+      ) : (
+        <div className="text-white">Cargando..</div>
       )}
+
+      <div className="join grid grid-cols-2 mt-10">
+        <button
+          className="join-item btn btn-outline bg-white"
+          onClick={handlePrevPage}
+        >
+          Anterior
+        </button>
+        <button
+          className="join-item btn btn-outline bg-white"
+          onClick={handleNextPage}
+        >
+          Siguiente
+        </button>
+      </div>
     </section>
   );
 };
