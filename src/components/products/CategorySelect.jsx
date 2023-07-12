14dbed136238e3e47a259489a9b713234b4545b6
Franco Buceta <francobuceta95@gmail.com>
2023-07-12 19:16:46 -0300

    add category selection & redirection

--- src/components/products/CategorySelect.jsx
+++ src/components/products/CategorySelect.jsx
@@ -1,26 +1,47 @@
+"use client"
+import { useState } from "react";
+import { useRouter } from 'next/navigation';
+
 const CategorySelect = () => {
+
+  const [category, setCategory] = useState("");
+  const { push } = useRouter();
+
+  const handleCategory = (e) => {
+    setCategory(e.target.value);
+  }
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if(category !== "") {
+      push(`/products/${category}`);
+    }
+  }
+
   return (
-    <section className="flex justify-center gap-10">
+    <section>
+      <form onSubmit={(e) => handleSubmit(e)} className="flex justify-center gap-10">
       <div className="form-control w-full max-w-xs">
         <label className="label">
           <span className="text-lg text-white">Elige una categoría:</span>
         </label>
-        <select defaultValue="Categoría" className="select select-bordered">
+        <select defaultValue="Categoría" onChange={(value) => handleCategory(value)} className="select select-bordered">
           <option disabled>Categoría</option>
-          <option>Teclados</option>
-          <option>Mouses</option>
-          <option>Auriculares y Micrófonos</option>
-          <option>Sillas Gamers</option>
-          <option>Cámaras Web</option>
-          <option>Pads</option>
-          <option>Volantes y gamepads</option>
-          <option>Parlantes y audio</option>
+          <option value="teclados">Teclados</option>
+          <option value="mouses">Mouses</option>
+          <option value="auriculares-y-microfonos">Auriculares y Micrófonos</option>
+          <option value="sillas-gamers">Sillas Gamers</option>
+          <option value="camaras-web">Cámaras Web</option>
+          <option value="pads">Pads</option>
+          <option value="volantes-y-gamepads">Volantes y gamepads</option>
+          <option value="parlantes-y-audio">Parlantes y audio</option>
         </select>
       </div>
 
       <div className="flex items-end">
-        <button className="btn bg-custome">Buscar</button>
+        <input type="submit" value="Buscar" className="btn bg-custome" />
       </div>
+      </form>
     </section>
   );
 };
