7e0960819a5c09e95dec0ce60a2e8c3777ac5924
Franco Buceta <francobuceta95@gmail.com>
2023-07-12 20:38:09 -0300

    move functionality to categories

--- src/app/products/page.jsx
+++ src/app/products/page.jsx
@@ -1,74 +1,7 @@
-"use client";
-import { useState, useEffect } from "react";
-import { getRequest } from "@/services/clientFetching";
-import ProductCard from "@/components/products/ProductCard";
-
-const Products = () => {
-  const [products, setProducts] = useState(null);
-  const [currentPage, setCurrentPage] = useState(1);
-
-  const handlePrevPage = () => {
-    if (currentPage > 1) {
-      setCurrentPage(currentPage - 1);
-    }
-  };
-
-  const handleNextPage = () => {
-    if (products.info.hasNextPage) {
-      setCurrentPage(currentPage + 1);
-    }
-  };
-
-  const fetchData = async (page) => {
-    try {
-      const response = await getRequest(`/api/products?page=${page}`);
-      setProducts(response);
-    } catch (error) {
-      console.log(error);
-    }
-  };
-
-  useEffect(() => {
-    fetchData(currentPage);
-    window.scrollTo({ top: 0, behavior: "smooth" });
-  }, [currentPage]);
-
+const Products = ({children}) => {
   return (
-    <section className="mt-12">
-      <div className="flex justify-center flex-wrap gap-8">
-        {products ? (
-          products.payload.map((product) => (
-            <ProductCard
-              key={product._id}
-              image={product.image}
-              title={product.title}
-              description={product.description}
-              price={product.price}
-            />
-          ))
-        ) : (
-          <div className="text-white">Cargando..</div>
-        )}
-      </div>
-
-      <div className="flex justify-center">
-        <div className="join grid grid-cols-2 mt-10">
-          <button
-            className="join-item btn btn-outline bg-white"
-            onClick={handlePrevPage}
-          >
-            Anterior
-          </button>
-          <button
-            className="join-item btn btn-outline bg-white"
-            onClick={handleNextPage}
-          >
-            Siguiente
-          </button>
-        </div>
-      </div>
-    </section>
-  );
-};
+    <div>{children}</div>
+  )
+}
 
 export default Products;
