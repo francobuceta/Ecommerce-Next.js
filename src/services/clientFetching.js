a898765896275db5ad31ae3c4f57691304e5d9a1
Franco Buceta <francobuceta95@gmail.com>
2023-07-12 18:24:38 -0300

    delete console.log

--- src/services/clientFetching.js
+++ src/services/clientFetching.js
@@ -2,7 +2,6 @@ const URL = process.env.NEXT_PUBLIC_BACKEND_API_URL;
 
 export const getRequest = async (endpoint) => {
   try {
-    console.log("me ejecute");
     const response = await fetch(URL + endpoint);
 
     return response.json();
