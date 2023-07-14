6fcf43d6e443b2ebff1658275298dc75071219a7
Franco Buceta <francobuceta95@gmail.com>
2023-07-14 10:22:36 -0300

    delete get request in social icons

--- src/components/auth/SocialIcons.jsx
+++ src/components/auth/SocialIcons.jsx
@@ -1,13 +1,16 @@
 import { AiOutlineGooglePlus, AiOutlineGithub } from "react-icons/ai";
 
 const SocialIcons = () => {
+
   return (
     <div className="my-1">
-      <div className="my-4 border border-solid border-[#ddd] rounded-full inline-flex justify-center items-center mx-1 w-[40px] h-[40px] cursor-pointer">
+      <div className="my-4 border border-solid border-[#ddd] rounded-full inline-flex justify-center items-center mx-1 w-[40px] h-[40px]">
         <AiOutlineGooglePlus size={20} />
       </div>
-      <div className="my-4 border border-solid border-[#ddd] rounded-full inline-flex justify-center items-center mx-1 w-[40px] h-[40px] cursor-pointer">
-        <AiOutlineGithub size={20} />
+      <div className="my-4 border border-solid border-[#ddd] rounded-full inline-flex justify-center items-center mx-1 w-[40px] h-[40px]">
+        <a href="https://backend-nodejs-self.vercel.app/user/registroGithub" rel="noopener noreferrer">
+          <AiOutlineGithub size={20} />
+        </a>
       </div>
     </div>
   );
