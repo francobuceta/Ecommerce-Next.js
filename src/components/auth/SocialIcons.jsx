ff82debc10abc4861757baaf64f42d09493df4f8
Franco Buceta <francobuceta95@gmail.com>
2023-07-15 18:09:36 -0300

    add google & github auth pop-up

--- src/components/auth/SocialIcons.jsx
+++ src/components/auth/SocialIcons.jsx
@@ -1,16 +1,39 @@
 import { AiOutlineGooglePlus, AiOutlineGithub } from "react-icons/ai";
 
 const SocialIcons = () => {
+  
+  const handleThirdPartyAuth = (path) => {
+    const popUp = window.open(
+      `https://backend-nodejs-self.vercel.app/user/${path}`,
+      "targetWindow",
+      `toolbar=no,
+      location=no,
+      status=no,
+      menubar=no,
+      scrollbars=yes,
+      resizable=yes,
+      width=620,
+      height=700`
+    );
+
+    window.addEventListener("message", (event) => {
+      if (event.origin === "https://backend-nodejs-self.vercel.app") {
+        if (event.data) {
+          console.log(event.data);
+
+          popUp?.close();
+        }
+      }
+    });
+  };
 
   return (
     <div className="my-1">
-      <div className="my-4 border border-solid border-[#ddd] rounded-full inline-flex justify-center items-center mx-1 w-[40px] h-[40px]">
-        <AiOutlineGooglePlus size={20} />
+      <div className="my-4 border border-solid border-[#ddd] rounded-full inline-flex justify-center items-center mx-1 w-[40px] h-[40px] cursor-pointer">
+        <AiOutlineGooglePlus size={20} onClick={() => {handleThirdPartyAuth("registroGoogle")}} />
       </div>
-      <div className="my-4 border border-solid border-[#ddd] rounded-full inline-flex justify-center items-center mx-1 w-[40px] h-[40px]">
-        <a href="https://backend-nodejs-self.vercel.app/user/registroGithub" rel="noopener noreferrer">
-          <AiOutlineGithub size={20} />
-        </a>
+      <div className="my-4 border border-solid border-[#ddd] rounded-full inline-flex justify-center items-center mx-1 w-[40px] h-[40px] cursor-pointer">
+        <AiOutlineGithub size={20} onClick={() => {handleThirdPartyAuth("registroGithub")}} />
       </div>
     </div>
   );
