7f5accd19b2c6a78c9f6b0f870488695df1eb698
Franco Buceta <francobuceta95@gmail.com>
2023-07-21 20:25:58 -0300

    add send product to cart button

---
+++ src/components/products/AddToCartButton.jsx
@@ -0,0 +1,11 @@
+"use client";
+
+const addToCartButton = () => {
+  return (
+    <button className="border border-none rounded-md w-52 text-xl text-white p-2 bg-custome-black hover:bg-white hover:text-black">
+      Agregar al carrito
+    </button>
+  );
+};
+
+export default addToCartButton;
