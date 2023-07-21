7afa603e9be387d3ed862c1e797fd7b73b077997
Franco Buceta <francobuceta95@gmail.com>
2023-07-21 20:26:06 -0300

    add buy button

---
+++ src/components/products/BuyButton.jsx
@@ -0,0 +1,11 @@
+"use client";
+
+const BuyButton = () => {
+  return (
+    <button className="border border-none rounded-md w-52 text-xl text-black p-2 bg-custome-primary hover:bg-white">
+      Comprar ahora
+    </button>
+  );
+};
+
+export default BuyButton;
