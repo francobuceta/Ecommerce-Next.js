3e51eca8d0c40fc581e5ec64f916e1c7359a0f22
Franco Buceta <francobuceta95@gmail.com>
2023-07-21 19:21:45 -0300

    add link to product detail

--- src/components/products/ProductCard.jsx
+++ src/components/products/ProductCard.jsx
@@ -1,7 +1,8 @@
 import Image from "next/image";
 import FavouriteButton from "./FavouriteButton";
+import Link from "next/link";
 
-const ProductCard = ({ image, title, description, price }) => {
+const ProductCard = ({ id, category, image, title, description, price }) => {
   return (
     <div className="card card-compact w-72 h-[400px] bg-base-100 shadow-xl">
       <figure className="relative p-20">
@@ -21,9 +22,11 @@ const ProductCard = ({ image, title, description, price }) => {
         <p className="truncate">{description}</p>
         <span className="text-xl font-bold">$ {price}</span>
         <div className="card-actions justify-end">
-          <button className="btn bg-transparent border border-custome-secondary w-20">
-            Ver
-          </button>
+          <Link href={`/products/${category}/${id}`}>
+            <button className="btn bg-transparent border border-custome-secondary w-20">
+              Ver
+            </button>
+          </Link>
         </div>
       </div>
     </div>
