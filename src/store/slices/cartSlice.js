0fbbd5a58bda49c0b7e1bfb649d22b24b6aa35d8
Franco Buceta <francobuceta95@gmail.com>
2023-08-05 14:50:04 -0300

    add refactor to addProductToCart function

--- src/store/slices/cartSlice.js
+++ src/store/slices/cartSlice.js
@@ -1,5 +1,8 @@
 import { createSlice } from "@reduxjs/toolkit";
-import { productToCartSucceeded, productToCartFailed } from "@/utils/Notifications";
+import {
+  productToCartSucceeded,
+  productToCartFailed,
+} from "@/utils/Notifications";
 
 const initialState = [];
 
@@ -7,19 +10,38 @@ const cartSlice = createSlice({
   name: "cart",
   initialState,
   reducers: {
+
     addProductToCart: (state, action) => {
-      let repeatedProduct = state.find(elem => elem.productId === action.payload.productId);
-      
-      if(!repeatedProduct) {
-        state.push(action.payload);
+      let repeatedProduct;
+
+      if (state.length > 0) {
+        repeatedProduct = state.find(
+          (elem) => elem.productId === action.payload.productId
+        );
+      }
+
+      if (!repeatedProduct) {
         productToCartSucceeded();
-      } else if (repeatedProduct && (repeatedProduct.quantity + action.payload.quantity) <= action.payload.stock) {
-        repeatedProduct.quantity += action.payload.quantity;
+        const newState = Array.isArray(state) ? [...state, action.payload] : [action.payload];
+        return newState;
+      } else if (
+        repeatedProduct &&
+        repeatedProduct.quantity + action.payload.quantity <=
+          action.payload.stock
+      ) {
         productToCartSucceeded();
+        return state.map(product =>
+          product.productId === action.payload.productId
+            ? {
+                ...product,
+                quantity: product.quantity + action.payload.quantity,
+              }
+            : product
+        );
       } else {
         productToCartFailed();
       }
-    },
+    }
   },
 });
 
