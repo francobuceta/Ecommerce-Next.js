f4826879067320c300930c84f02c3a7442618dd4
Franco Buceta <francobuceta95@gmail.com>
2023-09-07 21:03:47 -0300

    add account component

---
+++ src/components/navbar/Account.jsx
@@ -0,0 +1,53 @@
+"use client";
+import Link from "next/link";
+import { VscAccount } from "react-icons/vsc";
+import { useSelector, useDispatch } from "react-redux";
+import { logOutUser } from "@/store/slices/userSlice";
+import { HandleDropdown } from "@/utils/HandleDropdown";
+import { getRequest } from "@/services/clientFetching";
+
+const Account = () => {
+  const user = useSelector((state) => state.user);
+  const dispatch = useDispatch();
+
+  const handleLogOut = async () => {
+    HandleDropdown();
+    await getRequest("/user/logout");
+    dispatch(logOutUser());
+  }
+
+  return (
+    <div className="flex items-center gap-3">
+      <div className="dropdown dropdown-end">
+        <label tabIndex={0} className="cursor-pointer">
+          <VscAccount fontSize={30} color="white" />
+        </label>
+        <ul
+          tabIndex={0}
+          className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52"
+        >
+          {user.token !== "" ? (
+            <>
+              <li>
+                <Link href="/" onClick={HandleDropdown}>Favoritos</Link>
+              </li>
+              <li><span onClick={handleLogOut}>Cerrar Sesión</span></li>
+            </>
+          ) : (
+            <li>
+              <Link href="/auth" onClick={HandleDropdown}>Ingresar / Registrarse</Link>
+            </li>
+          )}
+        </ul>
+      </div>
+
+      {user && (
+        <div>
+          <span className="text-white">{user.firstName}</span>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default Account;
