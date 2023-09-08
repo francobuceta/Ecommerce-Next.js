99a7f4f4e75afd240e33292f9f676afefb2de417
Franco Buceta <francobuceta95@gmail.com>
2023-09-07 21:05:29 -0300

    add handle dropdown daisy ui component

---
+++ src/utils/HandleDropdown.js
@@ -0,0 +1,5 @@
+//Aplicamos esta función para poder cerrar el dropdown de DaisyUi al clickear un <li>
+export const HandleDropdown = () => {
+    const elem = document.activeElement;
+    elem?.blur();
+}
