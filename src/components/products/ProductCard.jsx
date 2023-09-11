ead1322219984e49aa1202b820ead3d6aab8aee9
Franco Buceta <francobuceta95@gmail.com>
2023-09-11 15:40:15 -0300

    add view transition api in product detail redirect

--- src/components/products/ProductCard.jsx
+++ src/components/products/ProductCard.jsx
@@ -1,9 +1,23 @@
 import Image from "next/image";
 import FavouriteButton from "./FavouriteButton";
-import Link from "next/link";
+import { useRouter } from "next/navigation";
+import { flushSync } from "react-dom";
 import { FormatNumber } from "@/utils/CartCounts";
 
 const ProductCard = ({ id, category, image, title, description, price }) => {
+
+  const router = useRouter();
+
+  //Redirection with transition to product detail.
+  const handleTransition = (category, productId) => {
+    if(!document.startViewTransition) {
+      router.push(`/products/${category}/${productId}`);
+    }
+    document.startViewTransition(() => {
+      flushSync(() => router.push(`/products/${category}/${productId}`));
+    });
+  }
+
   return (
     <div className="card card-compact w-72 h-[400px] bg-base-100 shadow-xl">
       <figure className="relative p-20">
@@ -19,15 +33,14 @@ const ProductCard = ({ id, category, image, title, description, price }) => {
       </figure>
 
       <div className="card-body bg-custome-primary rounded-b-2xl gap-0">
-        <h3 className="card-title select-none">{title}</h3>
+        <h3 className="truncate card-title select-none">{title}</h3>
         <p className="truncate">{description}</p>
         <span className="text-xl font-bold">{FormatNumber(price)}</span>
         <div className="card-actions justify-end">
-          <Link href={`/products/${category}/${id}`}>
-            <button className="btn bg-transparent border border-custome-secondary w-20">
-              Ver
-            </button>
-          </Link>
+          <button className="btn bg-transparent border border-custome-secondary w-20"
+            onClick={() => handleTransition(category, id)}>
+            Ver
+          </button>
         </div>
       </div>
     </div>
