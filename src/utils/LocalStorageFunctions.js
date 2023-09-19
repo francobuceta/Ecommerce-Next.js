f23aebcbf1f04c6384397fcb93fc07f74c5d87e0
Franco Buceta <francobuceta95@gmail.com>
2023-09-19 11:26:58 -0300

    fix local storage

--- src/utils/LocalStorageFunctions.js
+++ src/utils/LocalStorageFunctions.js
@@ -11,7 +11,7 @@ export const getLocalStorage = (key) => {
 };
 
 export const setLocalStorage = (key, value) => {
-  localStorage.setItem(key, JSON.stringify({ ...value }));
+  localStorage.setItem(key, JSON.stringify(value));
 };
 
 export const clearLocalStorage = (key) => {
