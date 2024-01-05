efc3d29c393a39ba1a6842fbcccc432eecef2f97
Franco Buceta <francobuceta95@gmail.com>
2024-01-05 18:12:47 -0300

    add redirectios to products page

--- src/components/home/Categories.jsx
+++ src/components/home/Categories.jsx
@@ -10,42 +10,42 @@ const categories = [
   {
     title: "Teclados",
     img: "/img/keyboard.jpg",
-    url: "/",
+    url: "/products/teclados",
   },
   {
     title: "Mouses",
     img: "/img/mouse.jpg",
-    url: "/",
+    url: "/products/mouses",
   },
   {
     title: "Auriculares y micrófonos",
     img: "/img/headphone.jpg",
-    url: "/",
+    url: "/products/auriculares-y-microfonos",
   },
   {
     title: "Sillas gamers",
     img: "/img/chair.jpg",
-    url: "/",
+    url: "/products/sillas-gamers",
   },
   {
     title: "Pads",
     img: "/img/pad.jpeg",
-    url: "/",
+    url: "/products/pads",
   },
   {
     title: "Cámaras web",
     img: "/img/webcam.jpg",
-    url: "/",
+    url: "/products/camaras-web",
   },
   {
     title: "Volantes y gamepads",
     img: "/img/joystick.jpg",
-    url: "/",
+    url: "/products/volantes-y-gamepads",
   },
   {
     title: "Parlantes y audio",
     img: "/img/phone-speaker.jpeg",
-    url: "/",
+    url: "/products/parlantes-y-audio",
   },
 ];
 
