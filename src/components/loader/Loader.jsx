130609e090946f598e01fef85cc0ff4066ded308
Franco Buceta <francobuceta95@gmail.com>
2024-01-08 15:58:57 -0300

    fix loader for checkout

--- src/components/loader/Loader.jsx
+++ src/components/loader/Loader.jsx
@@ -1,6 +1,8 @@
-const Loader = () => {
+const Loader = ( {checkout} ) => {
   return (
-    <div className="w-10 h-10 border-4 border-dashed rounded-full animate-spin border-custome-primary"></div>
+    <div className={`border-dashed rounded-full animate-spin border-custome-primary
+      ${checkout ? "w-20 h-20 border-[12px]" : "w-10 h-10 border-4"}`}>
+    </div>
   );
 };
 
