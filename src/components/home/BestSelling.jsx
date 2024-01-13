ea3f5fd3694c6a89c0847fa94871bef6f2eb1f67
Franco Buceta <francobuceta95@gmail.com>
2024-01-13 20:23:13 -0300

    add refactor in bestSelling array

--- src/components/home/BestSelling.jsx
+++ src/components/home/BestSelling.jsx
@@ -1,4 +1,5 @@
 "use client";
+import { bestSellingProducts } from "@/utils/BestSellingProducts";
 import { Swiper, SwiperSlide } from "swiper/react";
 import { EffectCoverflow } from "swiper";
 import { useRouter } from "next/navigation";
@@ -6,51 +7,6 @@ import { flushSync } from "react-dom";
 
 import "swiper/css";
 import "swiper/css/effect-coverflow";
-
-const slides = [
-  {
-    title: "Silla Corsair T1 RACE",
-    description: "Ergonomía | Comodidad | Ajustabilidad",
-    img: "/img/chair-corsair.jpeg",
-    url: "/products/sillas-gamers/64f890650bd5acd883524805",
-  },
-  {
-    title: "Auricular Redragon Lamia 2",
-    description: "Calidad | Portabilidad | Aislamiento",
-    img: "img/headphone-dragon.jpeg",
-    url: "/products/auriculares-y-microfonos/64f7c12bee0d88d7c7f97c5d",
-  },
-  {
-    title: "Teclado Galax Stealth 03",
-    description: "Sensibilidad | Comodidad | Ergonomía",
-    img: "./img/keyboard-galax.jpeg",
-    url: "/products/teclados/64ac15033572bd5ec6b11df1",
-  },
-  {
-    title: "Mouse Logitech G502X",
-    description: "Precisión | Durabilidad | Conectividad",
-    img: "/img/mouse-logi.jpeg",
-    url: "/products/mouses/64ad759ce573e7d0f33f7004",
-  },
-  {
-    title: "Parlante Logitech Z607",
-    description: "Conectividad | Potencia | Durabilidad",
-    img: "/img/phone-speaker-logi.jpeg",
-    url: "/products/parlantes-y-audio/64f8f2f901810797d0ef4a5d",
-  },
-  {
-    title: "Cámara IP Cloud Tp-Link Tapo C200",
-    description: "Calidad | Seguridad | Conectividad",
-    img: "/img/webcam-tp_link.jpeg",
-    url: "/products/camaras-web/64f8988341d8910056d1c22a",
-  },
-  {
-    title: "Volante Logitech G29",
-    description: "Sensibilidad | Durabilidad | Precisión",
-    img: "/img/steering-wheel-logi.jpeg",
-    url: "/products/volantes-y-gamepads/64f8ec721d5ccc58436c5a16",
-  },
-];
 
 const BestSelling = () => {
   const router = useRouter();
@@ -96,7 +52,7 @@ const BestSelling = () => {
         modules={[EffectCoverflow]}
       >
         <div className="swiper-wrapper">
-          {slides.map((slide) => (
+          {bestSellingProducts.map((slide) => (
             <SwiperSlide
               key={slide.title}
               style={{
---
+++ src/utils/BestSellingProducts.js
@@ -0,0 +1,44 @@
+export const bestSellingProducts = [
+    {
+      title: "Silla Corsair T1 RACE",
+      description: "Ergonomía | Comodidad | Ajustabilidad",
+      img: "/img/chair-corsair.jpeg",
+      url: "/products/sillas-gamers/64f890650bd5acd883524805",
+    },
+    {
+      title: "Auricular Redragon Lamia 2",
+      description: "Calidad | Portabilidad | Aislamiento",
+      img: "img/headphone-dragon.jpeg",
+      url: "/products/auriculares-y-microfonos/64f7c12bee0d88d7c7f97c5d",
+    },
+    {
+      title: "Teclado Galax Stealth 03",
+      description: "Sensibilidad | Comodidad | Ergonomía",
+      img: "./img/keyboard-galax.jpeg",
+      url: "/products/teclados/64ac15033572bd5ec6b11df1",
+    },
+    {
+      title: "Mouse Logitech G502X",
+      description: "Precisión | Durabilidad | Conectividad",
+      img: "/img/mouse-logi.jpeg",
+      url: "/products/mouses/64ad759ce573e7d0f33f7004",
+    },
+    {
+      title: "Parlante Logitech Z607",
+      description: "Conectividad | Potencia | Durabilidad",
+      img: "/img/phone-speaker-logi.jpeg",
+      url: "/products/parlantes-y-audio/64f8f2f901810797d0ef4a5d",
+    },
+    {
+      title: "Cámara IP Cloud Tp-Link Tapo C200",
+      description: "Calidad | Seguridad | Conectividad",
+      img: "/img/webcam-tp_link.jpeg",
+      url: "/products/camaras-web/64f8988341d8910056d1c22a",
+    },
+    {
+      title: "Volante Logitech G29",
+      description: "Sensibilidad | Durabilidad | Precisión",
+      img: "/img/steering-wheel-logi.jpeg",
+      url: "/products/volantes-y-gamepads/64f8ec721d5ccc58436c5a16",
+    },
+  ];
