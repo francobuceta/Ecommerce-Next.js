3efb6e8dc1a3b2d15c9541775dd91647535ff31f
Franco Buceta <francobuceta95@gmail.com>
2024-01-13 19:46:08 -0300

    add test for app performance in mobile

--- src/components/home/BestSelling.jsx
+++ src/components/home/BestSelling.jsx
@@ -74,17 +74,17 @@ const BestSelling = () => {
       <Swiper
         grabCursor
         centeredSlides
-        effect="coverflow"
+        //effect="coverflow"
         loop
         slideToClickedSlide={true}
         centeredSlidesBounds={true}
-        coverflowEffect={{
+        /* coverflowEffect={{
           rotate: 50,
           stretch: 0,
           depth: 100,
           modifier: 1,
           slideShadows: true,
-        }}
+        }} */
         breakpoints={{
           250: {
             slidesPerView: 2,
