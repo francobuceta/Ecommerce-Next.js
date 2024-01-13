e7e062138c18331d012f3133d30aa462dabc2251
Franco Buceta <francobuceta95@gmail.com>
2024-01-13 20:27:14 -0300

    add refactor in faqs array

--- src/components/home/Faqs.jsx
+++ src/components/home/Faqs.jsx
@@ -1,28 +1,4 @@
-const questions = [
-  {
-    id: 1,
-    title: "¿Cómo puedo contactar al servicio de atención al cliente?",
-    text: "Estamos disponibles las 24hs del día por cualquier consulta, a través de nuestras redes sociales.",
-  },
-  {
-    id: 2,
-    title: "¿Cuáles son los métodos de pago aceptados?",
-    text: "Todas las tarjetas.",
-  },
-  {
-    id: 3,
-    title: "¿Se realizan envíos a domicilio?",
-    text: "Una vez realizada la compra te llegará un mail a tu correo con los pasos a seguir para retirar la compra o enviarla a tu domicilio.",
-  },
-  {
-    id: 4,
-    title:
-      "¿Cuánto tiempo tarda en llegar mi pedido y cuál es el costo de envío?",
-    text: "Dentro de CABA, 3 días y el envío es gratis.",
-    text2: "Para provincia de Bs. As., 6 días y el costo es de $1.100.",
-    text3: "Para otras provincias,  10 días y el costo es de $2.500.",
-  },
-];
+import { questions } from "@/utils/Faqs";
 
 const Faqs = () => {
   return (
