ca3a8f6269c9aec37aee60907975582dd2980fc8
Franco Buceta <francobuceta95@gmail.com>
2024-01-23 20:11:43 -0300

    add styles for favourites products page

--- src/components/favourites/FavouritesComponent.jsx
+++ src/components/favourites/FavouritesComponent.jsx
@@ -17,39 +17,41 @@ const FavouritesComponent = () => {
   }, []);
 
   return (
-    <>
-      {favourite.length > 0 ? (
-        favourite.map((product) => (
-          <ProductCard
-            key={product.id}
-            id={product.id}
-            category={product.category}
-            image={product.image}
-            title={product.title}
-            description={product.description}
-            price={product.price}
-            favourite={favourite}
-            setFavourite={setFavourite}
-          />
-        ))
-      ) : (
-        <div className="flex flex-col justify-center items-center">
-          <BsFillSearchHeartFill className="w-[200px] sm:w-[300px] h-auto text-custome-primary" />
+    <section className="mt-12">
+      <div className="flex justify-center flex-wrap gap-8">
+        {favourite.length > 0 ? (
+          favourite.map((product) => (
+            <ProductCard
+              key={product.id}
+              id={product.id}
+              category={product.category}
+              image={product.image}
+              title={product.title}
+              description={product.description}
+              price={product.price}
+              favourite={favourite}
+              setFavourite={setFavourite}
+            />
+          ))
+        ) : (
+          <div className="flex flex-col justify-center items-center">
+            <BsFillSearchHeartFill className="w-[200px] sm:w-[300px] h-auto text-custome-primary" />
 
-          <div className="text-center mt-5">
-            <p className="text-white text-2xl px-10">
-              Todavía no agregaste ningún producto a favoritos.
-            </p>
+            <div className="text-center mt-5">
+              <p className="text-white text-2xl px-10">
+                Todavía no agregaste ningún producto a favoritos.
+              </p>
 
-            <Link href="/products/all">
-              <button className="btn btn-active bg-custome-primary text-xl text-custome-secondary border-none w-40 h-10 mt-3">
-                Ver Catálogo
-              </button>
-            </Link>
+              <Link href="/products/all">
+                <button className="btn btn-active bg-custome-primary text-xl text-custome-secondary border-none w-40 h-10 mt-3">
+                  Ver Catálogo
+                </button>
+              </Link>
+            </div>
           </div>
-        </div>
-      )}
-    </>
+        )}
+      </div>
+    </section>
   );
 };
 
