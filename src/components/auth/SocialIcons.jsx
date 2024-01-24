ae5158be800170b701dc7dbb32bd01f240f0b8b6
Franco Buceta <francobuceta95@gmail.com>
2024-01-24 20:03:50 -0300

    add styles on social icons hover

--- src/components/auth/SocialIcons.jsx
+++ src/components/auth/SocialIcons.jsx
@@ -1,5 +1,5 @@
 "use client";
-import { AiOutlineGooglePlus, AiOutlineGithub } from "react-icons/ai";
+import { AiOutlineGoogle, AiOutlineGithub } from "react-icons/ai";
 import { useDispatch } from "react-redux";
 import { setUserInStore } from "@/store/slices/userSlice";
 import { useRouter } from "next/navigation";
@@ -40,15 +40,15 @@ const SocialIcons = () => {
 
   return (
     <div className="my-1">
-      <div className="my-4 border border-solid border-[#ddd] rounded-full inline-flex justify-center items-center mx-1 w-[40px] h-[40px] cursor-pointer">
-        <AiOutlineGooglePlus
+      <div className="my-4 border border-solid border-[#ddd] rounded-full inline-flex justify-center items-center mx-1 w-[40px] h-[40px] cursor-pointer hover:text-[#DB4437] hover:bg-[#F4B400] hover:border-custome-secondary">
+        <AiOutlineGoogle
           size={20}
           onClick={() => {
             handleThirdPartyAuth("registroGoogle");
           }}
         />
       </div>
-      <div className="my-4 border border-solid border-[#ddd] rounded-full inline-flex justify-center items-center mx-1 w-[40px] h-[40px] cursor-pointer">
+      <div className="my-4 border border-solid border-[#ddd] rounded-full inline-flex justify-center items-center mx-1 w-[40px] h-[40px] cursor-pointer hover:text-[#FFFFFF] hover:bg-custome-secondary hover:border-custome-secondary">
         <AiOutlineGithub
           size={20}
           onClick={() => {
